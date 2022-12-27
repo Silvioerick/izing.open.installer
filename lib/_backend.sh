49653b2b85b78797082c9b49d4a1d364c72f9e22
Silvio <silvioerick@hotmail.com>
2022-12-27 21:02:03 +0000

    Correcao configuracao backend nginx

--- lib/_backend.sh
+++ lib/_backend.sh
@@ -297,7 +297,7 @@ server {
   server_name $backend_hostname;
 
   location / {
-    proxy_pass http://127.0.0.1:8080;
+    proxy_pass http://127.0.0.1:3000;
     proxy_http_version 1.1;
     proxy_set_header Upgrade \$http_upgrade;
     proxy_set_header Connection 'upgrade';
