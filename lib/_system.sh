fcb24386c942645f24e6fafa82ca0eb4384e7df7
Silvio <silvioerick@hotmail.com>
2022-12-28 18:22:43 +0000

    Atualizacao do Link da Repositorio do Durans

--- lib/_system.sh
+++ lib/_system.sh
@@ -35,7 +35,7 @@ system_git_clone() {
   sleep 2
 
   sudo su - deploy <<EOF
-  git clone https://github.com/ldurans/izing.io /home/deploy/izing.io/
+  git clone https://github.com/ldurans/izing.open.io /home/deploy/izing.io/
 EOF
 
   sleep 2
