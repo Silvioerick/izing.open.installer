283fd7d7d177a78a841954d88b31f6cd1a238a77
Silvio <silvioerick@hotmail.com>
2023-02-17 17:45:08 -0300

    correcao erro

--- lib/_backend.sh
+++ lib/_backend.sh
@@ -64,8 +64,8 @@ backend_postgres_create() {
   sudo su - root <<EOF
   usermod -aG docker deploy
   docker run --name postgresql \
-		-e POSTGRES_USER=$(db_user) \
-		-e POSTGRES_PASSWORD=$(db_pass) \
+		-e POSTGRES_USER=${db_user} \
+		-e POSTGRES_PASSWORD=${db_pass} \
                 -p 5432:5432 --restart=always \
                 -v /data:/var/lib/postgresql/data \
                 -d postgres \
@@ -161,7 +161,7 @@ JWT_SECRET=DPHmNRZWZ4isLF9vXkMv1QabvpcA80Rc
 JWT_REFRESH_SECRET=EMPehEbrAdi7s8fGSeYzqGQbV5wrjH4i
 
 IO_REDIS_SERVER=localhost
-IO_REDIS_PASSWORD='$(redis_pass)'
+IO_REDIS_PASSWORD='${redis_pass}'
 IO_REDIS_PORT='6379'
 IO_REDIS_DB_SESSION='2'
 
@@ -236,7 +236,7 @@ JWT_SECRET=DPHmNRZWZ4isLF9vXkMv1QabvpcA80Rc
 JWT_REFRESH_SECRET=EMPehEbrAdi7s8fGSeYzqGQbV5wrjH4i
 
 IO_REDIS_SERVER=localhost
-IO_REDIS_PASSWORD='password'
+IO_REDIS_PASSWORD='${redis_pass}'
 IO_REDIS_PORT='6379'
 IO_REDIS_DB_SESSION='2'
 
